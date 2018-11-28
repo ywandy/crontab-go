9ab0a8208bc4775774d6b186923745eca54ac024
ywandy <896882701yw@gmail.com>
2018-11-28 15:15:56 +0800

    加入了佛祖保佑

---
+++ common/ConsoleOutput.go
@@ -0,0 +1,33 @@
+package common
+
+import "fmt"
+
+
+//佛祖保佑,永无bug
+func MakeBuddhaBless(){
+	fmt.Println(" ......................我佛慈悲......................");
+	fmt.Println("                       _oo0oo_                      ");
+	fmt.Println("                      o8888888o                     ");
+	fmt.Println("                      88\" . \"88                     ");
+	fmt.Println("                      (| -_- |)                     ");
+	fmt.Println("                      0\\  =  /0                     ");
+	fmt.Println("                    ___/‘---’\\___                   ");
+	fmt.Println("                  .' \\|       |/ '.                 ");
+	fmt.Println("                 / \\\\|||  :  |||// \\                ");
+	fmt.Println("                / _||||| -卍-|||||_ \\               ");
+	fmt.Println("               |   | \\\\\\  -  /// |   |              ");
+	fmt.Println("               | \\_|  ''\\---/''  |_/ |              ");
+	fmt.Println("               \\  .-\\__  '-'  ___/-. /              ");
+	fmt.Println("             ___'. .'  /--.--\\  '. .'___            ");
+	fmt.Println("          .\"\" ‘<  ‘.___\\_<|>_/___.’ >’ \"\".          ");
+	fmt.Println("         | | :  ‘- \\‘.;‘\\ _ /’;.’/ - ’ : | |        ");
+	fmt.Println("         \\  \\ ‘_.   \\_ __\\ /__ _/   .-’ /  /        ");
+	fmt.Println("     =====‘-.____‘.___ \\_____/___.-’___.-’=====     ");
+	fmt.Println("                       ‘=---=’                      ");
+	fmt.Println("                                                    ");
+	fmt.Println("....................佛祖开光 ,永无BUG...................");
+}
+
+func Programintro(){
+	fmt.Println("高性能分布式 cron 服务")
+}
